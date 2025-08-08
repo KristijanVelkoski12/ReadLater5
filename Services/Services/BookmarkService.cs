3622aa6669b668db57ee8ace2f790ef3921188dc
Kristijan Velkoski <kristijan.velkoski@gmail.com>
2025-08-08 13:52:58 +0200

    'add bookmark service'

---
+++ Services/Services/BookmarkService.cs
@@ -0,0 +1,51 @@
+﻿using Data;
+using Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Services
+{
+    public class BookmarkService : IBookmarkService
+    {
+        private ReadLaterDataContext _ReadLaterDataContext;
+
+        public BookmarkService(ReadLaterDataContext readLaterDataContext)
+        {
+            _ReadLaterDataContext = readLaterDataContext;
+        }
+        public Bookmark CreateBookmark(Bookmark bookmark)
+        {
+            _ReadLaterDataContext.Add(bookmark);
+            _ReadLaterDataContext.SaveChanges();
+            return bookmark;
+        }
+
+        public void DeleteBookmark(Bookmark bookmark)
+        {
+            _ReadLaterDataContext.Update(bookmark);
+            _ReadLaterDataContext.SaveChanges();
+        }
+
+        public Bookmark GetBookmark(int Id)
+        {
+            return _ReadLaterDataContext.Bookmarks.Where(b => b.ID == Id).FirstOrDefault();
+        }
+
+        public Bookmark GetBookmarkByCategory(int CathegoryId)
+        {
+            return _ReadLaterDataContext.Bookmarks.Where(b => b.CategoryId == CathegoryId).FirstOrDefault();
+        }
+
+        public List<Bookmark> GetBookmarks()
+        {
+            return _ReadLaterDataContext.Bookmarks.ToList();
+        }
+
+        public void UpdateBookmark(Bookmark bookmark)
+        {
+            _ReadLaterDataContext.Update(bookmark);
+            _ReadLaterDataContext.SaveChanges();
+        }
+    }
+}
---
+++ Services/Services/IBookmarkService.cs
@@ -0,0 +1,15 @@
+﻿using Entity;
+using System.Collections.Generic;
+
+namespace Services
+{
+    public interface IBookmarkService
+    {
+        Bookmark CreateBookmark(Bookmark bookmark);
+        List<Bookmark> GetBookmarks();
+        Bookmark GetBookmark(int Id);
+        Bookmark GetBookmarkByCategory(int CathegoryId);
+        void UpdateBookmark(Bookmark bookmark);
+        void DeleteBookmark(Bookmark bookmark);
+    }
+}
